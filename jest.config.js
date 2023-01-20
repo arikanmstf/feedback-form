a6423322a4a0d78ac5dc09484a8e99f96843c45c
arikanmstf <arikanmstf@gmail.com>
2023-01-20 21:56:47 +0200

    fix jest tz issue

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,6 @@ module.exports = {
   globals: {
     window: {},
   },
+  globalSetup: '<rootDir>/global-setup.js',
   transformIgnorePatterns: [`<rootDir>/node_modules/(?!(${modulesToTransform.join('|')})).+\\.js$`],
 };
